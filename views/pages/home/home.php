5a72006775fd0fafe72d0badfeec438debdfbae2
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-09-21 13:25:14 -0300

    ui responsive changed,searchbar and categories on top

--- views/pages/home/home.php
+++ views/pages/home/home.php
@@ -9,28 +9,6 @@
 
 <div class="container">
     <div class="row">
-
-        <div class="col-lg-8">
-
-            <?php if (!empty($jobs)) {
-                include_once "../jobs/jobsCards.php";
-            } else {
-                echo "<h1>No jobs found! </h1>";
-            };
-            ?>
-            <!--    <nav aria-label="Pagination">
-                <hr class="my-0" />
-                <ul class="pagination justify-content-center my-4">
-                    <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a></li>
-                    <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#!">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#!">3</a></li>
-                    <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
-                    <li class="page-item"><a class="page-link" href="#!">15</a></li>
-                    <li class="page-item"><a class="page-link" href="#!">Older</a></li>
-                </ul>
-            </nav> -->
-        </div>
         <div class="col-lg-4">
             <div class="card mb-4">
                 <div class="card-header">Search</div>
@@ -56,5 +34,27 @@
             </div>
 
         </div>
+        <div class="col-lg-8">
+
+            <?php if (!empty($jobs)) {
+                include_once "../jobs/jobsCards.php";
+            } else {
+                echo "<h1>No jobs found! </h1>";
+            };
+            ?>
+            <!--    <nav aria-label="Pagination">
+                <hr class="my-0" />
+                <ul class="pagination justify-content-center my-4">
+                    <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a></li>
+                    <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#!">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#!">3</a></li>
+                    <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
+                    <li class="page-item"><a class="page-link" href="#!">15</a></li>
+                    <li class="page-item"><a class="page-link" href="#!">Older</a></li>
+                </ul>
+            </nav> -->
+        </div>
+
     </div>
 </div>
